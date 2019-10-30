802c640747dd07288e9806efe82a28abb54738b4
Alejandro Linan <jandrolinan@gmail.com>
2019-10-30 21:23:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,12 @@ def using_unshift(array, string)
   return array.unshift(string)
 end 
 
+def using_pop(array)
+  array.pop 
+  return array
+end 
+
+def pop_with_args(array)
+  new_array = array.pop
+  
+end 
